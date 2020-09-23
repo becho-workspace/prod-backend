53db96d6524ad2077b880ec6fdcddc35781a2656
TanishD <70093604+TanishD@users.noreply.github.com>
2020-09-23 22:07:31 +0530

    Update transaction.js

--- models/transaction.js
+++ models/transaction.js
@@ -2,9 +2,21 @@ const mongoose = require("mongoose");
 
 const transSchema = new mongoose.Schema(
     {
-        sellerId: String,
         buyerId: String,
-        priceAccepted: String,
+        buyerName: String,
+        buyerCity: String,
+        buyerAddress: String,
+        buyerContact: String,
+        sellerId: String,
+        sellerName: String,
+        sellerAddress: String,
+        sellerCity: String,
+        sellerContact:String,
+        itemName:String,
+        bidAcceptedDate: String,
+        bidAmount: Number,
+        payToCustomer: Number,
+        commission:Number,
         productId: String
     },
     { timestamps: true }
