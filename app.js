6a098ec01a6ffac7bd50149c3bbdcdf31c0dd36e
Mohit Madhukar Rakhade <48031017+mohitrakhade20@users.noreply.github.com>
2020-09-24 01:26:41 +0530

    update :bug:

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ const userRoutes = require("./routes/user");
 const categoryRoutes = require("./routes/category");
 const productRoutes = require("./routes/product");
 const orderRoutes = require("./routes/order");
+const transRoutes = require("./routes/transaction");
 
 
 //DB Connection
@@ -49,6 +50,7 @@ app.use("/api", userRoutes);
 app.use("/api", categoryRoutes);
 app.use("/api", productRoutes);
 app.use("/api", orderRoutes);
+app.use("/api", transRoutes);
 
 //PORT
 const port = process.env.PORT || 5000;
